4d0549650bfa2646fc2309d999ba83f56fca49f4
Exabiier <exabiier.bars@gmail.com>
2022-12-23 20:03:57 -0600

    Finishes the  About use functionality.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import './SpecialMenu.css';
+import {images, data } from '../../constants'
+import { SubHeading, MenuItem } from '../../components'
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu That Fits Your Pallatte" />
+      <h1 className="headText__cormorant">Today's Special</h1>
+    </div>
+    <div className="app__specialMenu-menu"
   </div>
 );
 
